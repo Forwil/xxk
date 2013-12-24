14d7e4f2fabdcb69b00e9ae5a80111dc9ccd9f5d
Forwil <yu10022@qq.com>
2013-12-25 01:32:32 +0800

    addjs

---
+++ static/demo.js
@@ -0,0 +1,9 @@
+	var get_image = function(){
+		var im = document.getElementById("image");
+		var src = document.getElementById("url");
+		im.src = src.value;
+		return false;
+	}
+	var check_null = function(){
+		var ;	
+	}
